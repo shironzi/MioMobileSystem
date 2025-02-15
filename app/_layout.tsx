5758210a7a4ac6496a31b053c93462fe9cd7672f
shironzi <aaronbaon1@gmail.com>
2025-02-15 20:26:32 +0800

    feat: add new screens for notifications and courses in layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(notification)" options={{ headerShown: false }} />
+      <Stack.Screen name="(course)" options={{ headerShown: false }} />
     </Stack>
   );
 }
