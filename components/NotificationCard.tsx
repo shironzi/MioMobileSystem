c84f058d921aee428f51437199475663fedaf1ae
shironzi <aaronbaon1@gmail.com>
2025-02-15 20:26:18 +0800

    feat: enhance NotificationCard component with dynamic props and icons; remove unused course and notification details components

--- components/NotificationCard.tsx
+++ components/NotificationCard.tsx
@@ -3,14 +3,89 @@ import { Card } from "@rneui/themed";
 import { Text, TouchableOpacity, View } from "react-native";
 import { useRouter } from "expo-router";
 import Foundation from "@expo/vector-icons/Foundation";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Entypo from "@expo/vector-icons/Entypo";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
-const NotificationCard = () => {
+const NotificationCard = (props: {
+  title: string;
+  date: Date;
+  time: string;
+  desc: string;
+  type: string;
+}) => {
   const router = useRouter();
-  const description =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusamus aperiam vel quas minima iure, obcaecati fuga eum blanditiis, error cupiditate sit exercitationem, tempora laboriosam nisi sed molestiae impedit omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit blanditiis officiis neque facilis veniam recusandae, dolore at inventore eum accusantium ut,tempora ad quos a dignissimos quae quam, odio tempore!";
+
+  const formatDate = (date: Date) => {
+    return date.toLocaleDateString("en-US", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    });
+  };
+
+  const isToday = (date: Date) => {
+    const today = new Date();
+    return date.toDateString() === today.toDateString();
+  };
+
+  const isYesterday = (date: Date) => {
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    return date.toDateString() === yesterday.toDateString();
+  };
+
+  const renderIcon = () => {
+    switch (props.type.toLowerCase()) {
+      case "activity":
+        return (
+          <MaterialIcons
+            name="event"
+            size={45}
+            color="black"
+            style={{ padding: 23 }}
+          />
+        );
+      case "warning":
+        return (
+          <Entypo
+            name="warning"
+            size={45}
+            color="black"
+            style={{ padding: 23 }}
+          />
+        );
+      case "message":
+        return (
+          <MaterialIcons
+            name="account-circle"
+            size={45}
+            color="black"
+            style={{ padding: 23 }}
+          />
+        );
+      case "fire":
+        return (
+          <FontAwesome5
+            name="fire"
+            size={45}
+            color="black"
+            style={{ padding: 23 }}
+          />
+        );
+      default:
+        return null;
+    }
+  };
 
   return (
-    <TouchableOpacity onPress={() => router.navigate("/course/courseDetails")}>
+    <TouchableOpacity
+      onPress={() =>
+        props.type.toLowerCase() === "message"
+          ? router.navigate("/notification/messageDetails")
+          : router.navigate("/notification/notificationDetails")
+      }
+    >
       <Card containerStyle={{ paddingLeft: 0, margin: 0 }}>
         <View
           style={{
@@ -21,12 +96,7 @@ const NotificationCard = () => {
             width: "100%",
           }}
         >
-          <Foundation
-            name="alert"
-            size={45}
-            color="black"
-            style={{ padding: 23 }}
-          />
+          {renderIcon()}
           <View>
             <View
               style={{
@@ -34,24 +104,35 @@ const NotificationCard = () => {
                 flexDirection: "row",
                 justifyContent: "space-between",
                 alignItems: "center",
-                width: "79%",
               }}
             >
-              <Text style={{ fontSize: 18 }}>Announcement Title</Text>
+              <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+                {props.title}
+              </Text>
               <View
                 style={{
                   display: "flex",
                   flexDirection: "row",
+                  justifyContent: "space-between",
+                  columnGap: 5,
                 }}
               >
-                <Text style={{ fontSize: 14 }}>Date</Text>
-                <Text style={{ fontSize: 14 }}>Time</Text>
+                <Text style={{ fontSize: 15 }}>
+                  {isToday(props.date)
+                    ? "Today"
+                    : isYesterday(props.date)
+                    ? "Yesterday"
+                    : formatDate(props.date)}
+                </Text>
+                <Text style={{ fontSize: 15 }}>{props.time}</Text>
               </View>
             </View>
-            <Text style={{ fontSize: 12, marginTop: 5, height: 60 }}>
-              {description.length > 100
-                ? description.substring(0, 150 - 3) + "..."
-                : description}
+            <Text
+              style={{ fontSize: 14, marginTop: 5, height: 60, width: 300 }}
+            >
+              {props.desc.length > 100
+                ? props.desc.substring(0, 150 - 3) + "..."
+                : props.desc}
             </Text>
           </View>
         </View>
