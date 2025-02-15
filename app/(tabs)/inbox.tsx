bb48591ae00cbc47fc60f23040a59d47ac93b927
shironzi <aaronbaon1@gmail.com>
2025-02-15 20:26:36 +0800

    feat: implement inbox screen with multiple NotificationCard components for displaying messages

--- app/(tabs)/inbox.tsx
+++ app/(tabs)/inbox.tsx
@@ -1,11 +1,92 @@
-import { View, Text } from "react-native";
+import { ScrollView } from "react-native";
 import React from "react";
+import NotificationCard from "@/components/NotificationCard";
 
 const inbox = () => {
   return (
-    <View>
-      <Text>inbox</Text>
-    </View>
+    <ScrollView>
+      <NotificationCard
+        title="Message"
+        date={new Date(Date.now())}
+        time="10:00 AM"
+        desc={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusamus aperiam vel quas minima iure, obcaecati fuga eum blanditiis, error cupiditate sit exercitationem, tempora laboriosam nisi sed molestiae impedit omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit blanditiis officiis neque facilis veniam recusandae, dolore at inventore eum accusantium ut,tempora ad quos a dignissimos quae quam, odio tempore!"
+        }
+        type="message"
+      />
+      <NotificationCard
+        title="Message"
+        date={new Date("2025-02-14")}
+        time="10:00 AM"
+        desc={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusamus aperiam vel quas minima iure, obcaecati fuga eum blanditiis, error cupiditate sit exercitationem, tempora laboriosam nisi sed molestiae impedit omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit blanditiis officiis neque facilis veniam recusandae, dolore at inventore eum accusantium ut,tempora ad quos a dignissimos quae quam, odio tempore!"
+        }
+        type="message"
+      />
+      <NotificationCard
+        title="Message"
+        date={new Date("2025-02-13")}
+        time="10:00 AM"
+        desc={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusamus aperiam vel quas minima iure, obcaecati fuga eum blanditiis, error cupiditate sit exercitationem, tempora laboriosam nisi sed molestiae impedit omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit blanditiis officiis neque facilis veniam recusandae, dolore at inventore eum accusantium ut,tempora ad quos a dignissimos quae quam, odio tempore!"
+        }
+        type="message"
+      />
+      <NotificationCard
+        title="Message"
+        date={new Date("2025-02-12")}
+        time="10:00 AM"
+        desc={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusamus aperiam vel quas minima iure, obcaecati fuga eum blanditiis, error cupiditate sit exercitationem, tempora laboriosam nisi sed molestiae impedit omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit blanditiis officiis neque facilis veniam recusandae, dolore at inventore eum accusantium ut,tempora ad quos a dignissimos quae quam, odio tempore!"
+        }
+        type="message"
+      />
+      <NotificationCard
+        title="Message"
+        date={new Date(Date.now())}
+        time="10:00 AM"
+        desc={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusamus aperiam vel quas minima iure, obcaecati fuga eum blanditiis, error cupiditate sit exercitationem, tempora laboriosam nisi sed molestiae impedit omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit blanditiis officiis neque facilis veniam recusandae, dolore at inventore eum accusantium ut,tempora ad quos a dignissimos quae quam, odio tempore!"
+        }
+        type="message"
+      />
+      <NotificationCard
+        title="Message"
+        date={new Date(Date.now())}
+        time="10:00 AM"
+        desc={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusamus aperiam vel quas minima iure, obcaecati fuga eum blanditiis, error cupiditate sit exercitationem, tempora laboriosam nisi sed molestiae impedit omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit blanditiis officiis neque facilis veniam recusandae, dolore at inventore eum accusantium ut,tempora ad quos a dignissimos quae quam, odio tempore!"
+        }
+        type="message"
+      />
+      <NotificationCard
+        title="Message"
+        date={new Date(Date.now())}
+        time="10:00 AM"
+        desc={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusamus aperiam vel quas minima iure, obcaecati fuga eum blanditiis, error cupiditate sit exercitationem, tempora laboriosam nisi sed molestiae impedit omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit blanditiis officiis neque facilis veniam recusandae, dolore at inventore eum accusantium ut,tempora ad quos a dignissimos quae quam, odio tempore!"
+        }
+        type="message"
+      />
+      <NotificationCard
+        title="Message"
+        date={new Date(Date.now())}
+        time="10:00 AM"
+        desc={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusamus aperiam vel quas minima iure, obcaecati fuga eum blanditiis, error cupiditate sit exercitationem, tempora laboriosam nisi sed molestiae impedit omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit blanditiis officiis neque facilis veniam recusandae, dolore at inventore eum accusantium ut,tempora ad quos a dignissimos quae quam, odio tempore!"
+        }
+        type="message"
+      />
+      <NotificationCard
+        title="Message"
+        date={new Date(Date.now())}
+        time="10:00 AM"
+        desc={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusamus aperiam vel quas minima iure, obcaecati fuga eum blanditiis, error cupiditate sit exercitationem, tempora laboriosam nisi sed molestiae impedit omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit blanditiis officiis neque facilis veniam recusandae, dolore at inventore eum accusantium ut,tempora ad quos a dignissimos quae quam, odio tempore!"
+        }
+        type="message"
+      />
+    </ScrollView>
   );
 };
 
