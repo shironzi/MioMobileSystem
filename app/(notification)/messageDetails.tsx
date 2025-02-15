258de5b12023ad34286dfd586ea84711aa21186d
shironzi <aaronbaon1@gmail.com>
2025-02-15 20:26:25 +0800

    feat: add messageDetails component for displaying message content

---
+++ app/(notification)/messageDetails.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const messageDetails = () => {
+  return (
+    <View>
+      <Text>messageDetails</Text>
+    </View>
+  );
+};
+
+export default messageDetails;
