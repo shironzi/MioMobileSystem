0b84ef0960a52c8e429b096ce54e28d2ac0ddc98
shironzi <aaronbaon1@gmail.com>
2025-02-17 00:03:16 +0800

    refactor: enhance tab bar icon functionality and improve screen options

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -9,8 +9,9 @@ import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 export default function Layout() {
   return (
     <Tabs
-      screenOptions={{
-        // tabBarActiveTintColor: "yellow",
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "#CCC",
         tabBarStyle: {
           backgroundColor: "#2264DC",
           borderTopLeftRadius: 15,
@@ -19,44 +20,77 @@ export default function Layout() {
           paddingTop: 10,
           paddingBottom: 10,
         },
-        headerLeft: () => <DrawerToggleButton tintColor="white"/>,
-      }}
+        headerLeft: () => <DrawerToggleButton tintColor="white" />,
+      })}
     >
       <Tabs.Screen
         name="index"
         options={{
-          tabBarLabel: () => <Text style={{ color: "white" }}>Dashboard</Text>,
-          tabBarIcon: () => <Ionicons name="apps" size={20} color="white" />, 
-          headerTitle: () => <Text style={{ color: "white", fontSize: 18 }}>Dashboard</Text>,
-          headerBackground: () => <View style={{ backgroundColor: "#2264DC", flex: 1 }} />,
+          tabBarLabel: () => (
+            <Text
+              style={{
+                color: "white",
+              }}
+            >
+              Dashboard
+            </Text>
+          ),
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="apps" size={20} color={color} />
+          ),
+          headerTitle: () => (
+            <Text style={{ color: "white", fontSize: 18 }}>Dashboard</Text>
+          ),
+          headerBackground: () => (
+            <View style={{ backgroundColor: "#2264DC", flex: 1 }} />
+          ),
         }}
       />
       <Tabs.Screen
         name="todo"
         options={{
           tabBarLabel: () => <Text style={{ color: "white" }}>To-do</Text>,
-          tabBarIcon: () => <MaterialIcons name="checklist" size={24} color="white" />, 
-          headerTitle: () => <Text style={{ color: "white", fontSize: 18 }}>To-do</Text>,
-          headerBackground: () => <View style={{ backgroundColor: "#2264DC", flex: 1 }} />,
-          
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="checklist" size={24} color={color} />
+          ),
+          headerTitle: () => (
+            <Text style={{ color: "white", fontSize: 18 }}>To-do</Text>
+          ),
+          headerBackground: () => (
+            <View style={{ backgroundColor: "#2264DC", flex: 1 }} />
+          ),
         }}
       />
       <Tabs.Screen
         name="notification"
         options={{
-          tabBarLabel: () => <Text style={{ color: "white" }}>Notification</Text>,
-          tabBarIcon: () => <Ionicons name="notifications" size={24} color="white" />,
-          headerTitle: () => <Text style={{ color: "white", fontSize: 18}}>Notification</Text>,
-          headerBackground: () => <View style={{ backgroundColor: "#2264DC", flex: 1 }} />,
+          tabBarLabel: () => (
+            <Text style={{ color: "white" }}>Notification</Text>
+          ),
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="notifications" size={24} color={color} />
+          ),
+          headerTitle: () => (
+            <Text style={{ color: "white", fontSize: 18 }}>Notification</Text>
+          ),
+          headerBackground: () => (
+            <View style={{ backgroundColor: "#2264DC", flex: 1 }} />
+          ),
         }}
       />
       <Tabs.Screen
         name="inbox"
         options={{
           tabBarLabel: () => <Text style={{ color: "white" }}>Inbox</Text>,
-          tabBarIcon: () => <Feather name="mail" size={24} color="white" />,
-          headerTitle: () => <Text style={{ color: "white", fontSize: 18}}>Inbox</Text>,
-          headerBackground: () => <View style={{ backgroundColor: "#2264DC", flex: 1 }} />,
+          tabBarIcon: ({ color }) => (
+            <Feather name="mail" size={24} color={color} />
+          ),
+          headerTitle: () => (
+            <Text style={{ color: "white", fontSize: 18 }}>Inbox</Text>
+          ),
+          headerBackground: () => (
+            <View style={{ backgroundColor: "#2264DC", flex: 1 }} />
+          ),
         }}
       />
     </Tabs>
