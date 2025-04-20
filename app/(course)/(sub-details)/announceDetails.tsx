31813e3fedf307bc2b90527a46cd265e15965b1d
notsoeyvah <avarce15@gmail.com>
2025-04-21 02:23:12 +0800

    Ass and Announcement Layout

---
+++ app/(course)/(sub-details)/announceDetails.tsx
@@ -0,0 +1,104 @@
+import { View, Text, StyleSheet } from "react-native";
+import React, { memo, useCallback } from "react";
+import { useFocusEffect, useNavigation } from "expo-router";
+import { Card } from "@rneui/themed";
+
+const data = [
+  {
+    id: 1,
+    title: "Activity 1",
+    date: new Date(Date.now()),
+    time: "10:00 AM",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisice elit. Quisquam, voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+  },
+];
+
+const announceDetails = () => {
+  const navigation = useNavigation();
+
+  useFocusEffect(
+    useCallback(() => {
+      navigation.setOptions({
+        headerTitle: "Announcement Details",
+        headerStyle: {
+          backgroundColor: "#2264DC",
+        },
+        headerTintColor: "#fff",
+      });
+
+      return () => {
+        navigation.setOptions({
+          headerTitle: "",
+          headerStyle: {
+            backgroundColor: "",
+          },
+          headerTintColor: "",
+        });
+      };
+    }, [navigation])
+  );
+
+  return (
+    <View style={styles.container}>
+      {data.map((item) => (
+        <Card key={item.id} containerStyle={styles.cardContainer}>
+          <View style={styles.cardContent}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.date}>
+              {item.date.toLocaleDateString("en-US", {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+              })}{" "}
+              {item.time}
+            </Text>
+            <Text style={styles.description}>{item.desc}</Text>
+          </View>
+        </Card>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    padding: 10,
+  },
+  cardContainer: {
+    padding: 15,
+    margin: 10,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    borderWidth: 0,
+    shadowColor: "transparent",
+  },
+  cardContent: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+  },
+  title: {
+    fontSize: 18,
+    color: "#000",
+    marginBottom: 5,
+  },
+  date: {
+    fontSize: 14,
+    color: "#888",
+    marginBottom: 30,
+  },
+  description: {
+    fontSize: 16,
+    color: "#333",
+    textAlign: "left",
+    left: 2,
+    lineHeight: 20,
+    width: "100%", 
+    borderColor: "#FFBF18",
+    paddingLeft: 0,
+  },
+});
+
+export default memo(announceDetails);
---
+++ components/AnnounceCard.tsx
@@ -0,0 +1,88 @@
+import React, { memo, useCallback } from "react";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import { Card } from "@rneui/themed";
+import { useRouter } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+
+const announceCard = (props: { 
+    title: string; 
+    date: Date; 
+    time: string 
+}) => {
+  const router = useRouter();
+
+  const formatDate = useCallback(
+    (date: Date) => {
+      return date.toLocaleDateString("en-US", {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      });
+    },
+    [Date]
+  );
+
+  return (
+    <TouchableOpacity
+      onPress={() => router.navigate("/(sub-details)/announceDetails")}
+      style={styles.touchableOpacity}
+    >
+      <Card containerStyle={styles.cardContainer}>
+        <View style={[styles.row, styles.linkDecoration]}>
+          <Text style={styles.title}>{props.title}</Text>
+          <View style={styles.rightSection}>
+            <Text style={styles.date}>
+              {formatDate(props.date)} {props.time}
+            </Text>
+            <Entypo name="chevron-small-right" size={30} color="#ccc" />
+          </View>
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  touchableOpacity: {
+    backgroundColor: "#fff",
+    padding: 15,
+    paddingEnd: 0,
+    margin: 15,
+    marginBottom: 0,
+    borderRadius: 10,
+  },
+  cardContainer: {
+    padding: 0,
+    margin: 0,
+    borderWidth: 0, 
+    shadowColor: "transparent",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 18,
+  },
+  date: {
+    fontSize: 12,
+    color: "#888",
+  },
+  linkDecoration: {
+    borderLeftColor: "#FFBF18",
+    borderLeftWidth: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  rightSection: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    right: -10,
+  },
+});
+
+export default memo(announceCard);
