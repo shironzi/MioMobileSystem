d49f30f8cd9259baf0b0dd8822f168038eea12d3
notsoeyvah <avarce15@gmail.com>
2025-04-22 17:55:13 +0800

    feat: implement calendar screen with localization and calendar card component

--- app/calendar.tsx
+++ app/calendar.tsx
@@ -1,12 +1,166 @@
-import { View, Text } from "react-native";
-import React, { memo } from "react";
+import React, { memo, useCallback, useState } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+import { useFocusEffect, useNavigation } from "expo-router";
+import { Calendar, LocaleConfig, DateData } from "react-native-calendars";
+import CalendarCard from "@/components/CalendarCard";
+import { MarkedDates } from "react-native-calendars/src/types";
 
-const calendar = () => {
+LocaleConfig.locales["fr"] = {
+  monthNames: [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December",
+  ],
+  monthNamesShort: [
+    "Jan", "Feb", "Mar", "Apr", "May", "June",
+    "July", "Aug", "Sept", "Oct", "Nov", "Dec",
+  ],
+  dayNames: [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
+  ],
+  dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+  today: "Today",
+};
+LocaleConfig.defaultLocale = "fr";
+
+const CalendarScreen = () => {
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState("");
+
+  useFocusEffect(
+    useCallback(() => {
+      navigation.setOptions({
+        headerTitle: "Calendar",
+        headerStyle: {
+          backgroundColor: "#2264DC",
+        },
+        headerTintColor: "#fff",
+      });
+
+      return () => {
+        navigation.setOptions({
+          headerTitle: "",
+          headerStyle: {
+            backgroundColor: "",
+          },
+          headerTintColor: "",
+        });
+      };
+    }, [navigation])
+  );
+
+  const markedDates: MarkedDates = selected
+    ? {
+        [selected]: {
+          selected: true,
+          selectedColor: "#FFBF18",
+          selectedTextColor: "#fff",
+          dots: [
+            {
+              key: "dot1",
+              color: "#2264DC",
+              selectedDotColor: "#2264DC",
+            },
+          ],
+        },
+      }
+    : {};
+
   return (
-    <View>
-      <Text>calendar</Text>
+    <View style={styles.container}>
+      <Calendar
+        style={styles.calendar}
+        markingType="multi-dot"
+        markedDates={markedDates}
+        enableSwipeMonths={true}
+        theme={{
+          backgroundColor: "#ffffff",
+          calendarBackground: "#ffffff",
+          textSectionTitleColor: "#b6c1cd",
+          selectedDayBackgroundColor: "#FFBF18",
+          selectedDayTextColor: "#000",
+          todayTextColor: "#FFBF18",
+          dayTextColor: "#2d4150",
+          textDisabledColor: "#d9e1e8",
+          dotColor: "#2264DC",
+          selectedDotColor: "#ffffff",
+          arrowColor: "#2264DC",
+          monthTextColor: "#2264DC",
+          indicatorColor: "#2264DC",
+          textDayFontWeight: "300",
+          textMonthFontWeight: "bold",
+          textDayHeaderFontWeight: "300",
+          textDayFontSize: 16,
+          textMonthFontSize: 18,
+          textDayHeaderFontSize: 14,
+        }}
+        onDayPress={(day: DateData) => {
+          setSelected(day.dateString);
+        }}
+      />
+       <ScrollView contentContainerStyle={styles.scrollContainer}>
+        {selected && (
+          <>
+            <CalendarCard
+              title="Speech Development"
+              sub="Picture Flashcards"
+              date={selected}
+              time="10:00 AM"
+              type="Submitted"
+            />
+            <CalendarCard
+              title="Speech Development"
+              sub="Picture Flashcards"
+              date={selected}
+              time="10:00 AM"
+              type="Submitted"
+            />
+            <CalendarCard
+              title="Speech Development"
+              sub="Picture Flashcards"
+              date={selected}
+              time="10:00 AM"
+              type="Submitted"
+            />
+            <CalendarCard
+              title="Speech Development"
+              sub="Picture Flashcards"
+              date={selected}
+              time="10:00 AM"
+              type="Submitted"
+            />
+            <CalendarCard
+              title="Speech Development"
+              sub="Picture Flashcards"
+              date={selected}
+              time="10:00 AM"
+              type="Submitted"
+            />
+          </>
+        )}
+      </ScrollView>
     </View>
   );
 };
 
-export default memo(calendar);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 10,
+  },
+  calendar: {
+    // borderRadius: 10,
+    // elevation: 5,
+    // shadowColor: "#000",
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.2,
+    // shadowRadius: 4,
+    margin: 5,
+  },
+  scrollContainer: {
+    paddingHorizontal: 10,
+    paddingBottom: 70,
+  },
+});
+
+export default memo(CalendarScreen);
---
+++ components/CalendarCard.tsx
@@ -0,0 +1,102 @@
+import React, { memo } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Card } from "@rneui/themed";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+type CalendarCardProps = {
+  sub: string;
+  title: string;
+  date: string;
+  time: string;
+  type: string;
+};
+
+const CalendarCard: React.FC<CalendarCardProps> = (props) => {
+  const formatDate = (date: string) => {
+    return new Date(date).toLocaleDateString("en-US", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    });
+  };
+
+  return (
+    <TouchableOpacity>
+      <Card containerStyle={styles.card}>
+        <View style={styles.cardContent}>
+          <MaterialIcons name="image" size={50} color="#FFBF18" style={styles.icon} />
+          <Text style={styles.sub}>{props.sub}</Text>
+          <Text style={styles.title}>{props.title}</Text>
+          <View style={styles.row}>
+            <Text style={styles.date}>{formatDate(props.date)}</Text>
+            <Text style={styles.time}> | {props.time}</Text>
+          </View>
+          <Text style={styles.type}>{props.type}</Text>
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    left: 0,
+    width: "92%",
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+    padding: 20,
+    height: 110,
+    
+  },
+  cardContent: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  title: {
+    left: 70,
+    top: -50,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#2264DC",
+    marginBottom: 2,
+  },
+  date: {
+    fontSize: 14,
+    color: "#666",
+  },
+  sub: {
+    left: 70,
+    top: -50,
+    fontSize: 14,
+    color: "#666",
+  },
+  time: {
+    fontSize: 14,
+    color: "#666",
+  },
+  type: {
+    left: 70,
+    top: -50,
+    fontSize: 14,
+    color: "#FFBF18",
+  },
+  row: {
+    left: 70,
+    top: -50,
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 2,
+  },
+  icon: {
+    marginRight: 10,
+    left: 0,
+    top: 20,
+  },
+});
+
+export default memo(CalendarCard);
