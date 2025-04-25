f7982d3c146d9ad784eb1321db8b6ebd1190fd3a
josh <aaronbaon1@gmail.com>
2025-04-25 20:59:32 +0800

    refactor: added progress bar

--- app/(course)/(speech)/picgame.tsx
+++ app/(course)/(speech)/picgame.tsx
@@ -27,34 +27,69 @@ const pictureFlashcard = [
 ];
 
 const picgame = () => {
-   const navigation = useNavigation();
-   useFocusEffect(
-      useCallback(() => {
-        navigation.setOptions({
-          headerTitle: "Picture Flashcards",
-          headerStyle: {
-            backgroundColor: "#2264DC",
-          },
-          headerTintColor: "#fff",
-        });
-  
-        return () => {
-          navigation.setOptions({
-            headerTitle: "",
-            headerStyle: {
-              backgroundColor: "",
-            },
-            headerTintColor: "",
-          });
-        };
-      }, [navigation])
-    );
-    return (
-        <View>
-            <Text>Hi</Text>
-        </View>
-    )
-}
+  const navigation = useNavigation();
+  useFocusEffect(
+    useCallback(() => {
+      navigation.setOptions({
+        headerTitle: "Picture Flashcards",
+        headerStyle: {
+          backgroundColor: "#2264DC",
+        },
+        headerTintColor: "#fff",
+      });
 
+      return () => {
+        navigation.setOptions({
+          headerTitle: "",
+          headerStyle: {
+            backgroundColor: "",
+          },
+          headerTintColor: "",
+        });
+      };
+    }, [navigation])
+  );
+  return (
+    <View style={{ marginHorizontal: 23 }}>
+      <View>
+        <Text
+          style={{
+            fontSize: 17,
+            color: "#1F1F1F",
+            marginTop: 13,
+            fontWeight: 800,
+          }}
+        >
+          Easy
+        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            marginTop: 13,
+            gap: 10,
+          }}
+        >
+          {data?.map((ques, index) => (
+            <View
+              key={index}
+              style={{
+                flex: 1,
+                borderColor: "#CBCBCB",
+                borderWidth: 1,
+                backgroundColor: "#CBCBCB",
+                borderRadius: 5,
+                height: 25,
+                width: "auto",
+              }}
+            />
+          ))}
+        </View>
+        <Text style={{ color: "#434242", fontSize: 15, marginTop: 25 }}>
+          Guess the picture
+        </Text>
+      </View>
+    </View>
+  );
+};
 
 export default memo(picgame);
