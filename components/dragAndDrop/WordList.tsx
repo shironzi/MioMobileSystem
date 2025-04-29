cdb8c6eb1cc191a85d44d65c0b3b4308886d2deb
josh <aaronbaon1@gmail.com>
2025-04-30 01:47:16 +0800

    feat: enhance WordList component with dynamic layout and sentence tracking

--- components/dragAndDrop/WordList.tsx
+++ components/dragAndDrop/WordList.tsx
@@ -1,36 +1,131 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, {
+  ReactElement,
+  useEffect,
+  useMemo,
+  useState,
+  useRef,
+} from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
+import { useSharedValue, runOnUI, runOnJS } from "react-native-reanimated";
 
-type WordListProps = {
-  children: React.ReactNode;
-  title?: string;
-};
+import SortableWord from "./SortableWord";
+import Lines from "./Lines";
+import { MARGIN_LEFT } from "./Layout";
 
-const WordList = ({ children, title }: WordListProps) => {
-  return (
-    <View style={styles.container}>
-      {title && <Text style={styles.title}>{title}</Text>}
-      <View style={styles.wordsContainer}>{children}</View>
-    </View>
-  );
-};
-
+const containerWidth = Dimensions.get("window").width * 0.8;
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
-    borderRadius: 10,
+    flex: 1,
+    margin: MARGIN_LEFT,
+    marginTop: 0,
   },
-  title: {
-    fontSize: 16,
-    fontWeight: "bold",
-    marginBottom: 8,
-  },
-  wordsContainer: {
+  row: {
+    flex: 1,
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "center",
-    gap: 8,
+    opacity: 1,
   },
 });
+
+interface WordListProps {
+  children: ReactElement<{ id: number; word: string }>[];
+  onSentenceChange: (sentence: string) => void;
+}
+
+const WordList = ({ children, onSentenceChange }: WordListProps) => {
+  const [ready, setReady] = useState(false);
+  const previousSentenceRef = useRef<string>("");
+
+  const offsets = children.map(() => ({
+    order: useSharedValue(0),
+    width: useSharedValue(0),
+    height: useSharedValue(0),
+    x: useSharedValue(0),
+    y: useSharedValue(0),
+    originalX: useSharedValue(0),
+    originalY: useSharedValue(0),
+  }));
+
+  useEffect(() => {
+    if (ready) {
+      const interval = setInterval(() => {
+        const currentOrder = [];
+        for (let i = 0; i < offsets.length; i++) {
+          if (offsets[i].order.value >= 0) {
+            currentOrder.push({
+              word: children[i].props.word,
+              order: offsets[i].order.value,
+            });
+          }
+        }
+
+        currentOrder.sort((a, b) => a.order - b.order);
+        const newSentence = currentOrder.map((item) => item.word).join(" ");
+
+        if (newSentence !== previousSentenceRef.current) {
+          previousSentenceRef.current = newSentence;
+
+          if (onSentenceChange) {
+            onSentenceChange(newSentence);
+          }
+        }
+      }, 700);
+
+      return () => clearInterval(interval);
+    }
+  }, [ready, offsets, children, onSentenceChange]);
+
+  if (!ready) {
+    return (
+      <View style={styles.row}>
+        {children.map((child, index) => {
+          return (
+            <View
+              key={index}
+              onLayout={({
+                nativeEvent: {
+                  layout: { x, y, width, height },
+                },
+              }) => {
+                const offset = offsets[index]!;
+                offset.order.value = -1;
+                offset.width.value = width;
+                offset.height.value = height;
+                offset.originalX.value = x;
+                offset.originalY.value = y;
+                runOnUI(() => {
+                  "worklet";
+                  if (
+                    offsets.filter((o) => o.order.value !== -1).length === 0
+                  ) {
+                    runOnJS(setReady)(true);
+                  }
+                })();
+              }}
+            >
+              {child}
+            </View>
+          );
+        })}
+      </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <Lines />
+      {children.map((child, index) => (
+        <SortableWord
+          key={index}
+          offsets={offsets}
+          index={index}
+          containerWidth={containerWidth}
+        >
+          {child}
+        </SortableWord>
+      ))}
+    </View>
+  );
+};
 
 export default WordList;
