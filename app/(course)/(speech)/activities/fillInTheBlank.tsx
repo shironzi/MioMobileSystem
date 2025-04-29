841b9cb7188f663089352d532e2c3079bcb24842
josh <aaronbaon1@gmail.com>
2025-04-30 01:46:27 +0800

    refactor: update the component and enhance layout

--- app/(course)/(speech)/activities/fillInTheBlank.tsx
+++ app/(course)/(speech)/activities/fillInTheBlank.tsx
@@ -1,19 +1,19 @@
-import { View, StyleSheet, Dimensions } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Text,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import React, { useCallback, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { FontAwesome6 } from "@expo/vector-icons";
 import { useFocusEffect } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import ActivityProgress from "@/components/activityProgress";
 import Word from "@/components/dragAndDrop/Word";
-import { Box, Dropzone } from "./types";
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-} from "react-native-reanimated";
-import {
-  GestureHandlerRootView,
-  PanGestureHandler,
-} from "react-native-gesture-handler";
+import WordList from "@/components/dragAndDrop/WordList";
 
 const words = [
   { id: 1, word: "Can" },
@@ -28,19 +28,23 @@ const words = [
   { id: 10, word: "notebook" },
 ];
 
-const { width, height } = Dimensions.get("window");
-const BOX_SIZE = 80;
-const DROP_ZONE_COUNT = 4;
-const DROP_ZONE_SPACING =
-  (width - DROP_ZONE_COUNT * BOX_SIZE) / (DROP_ZONE_COUNT + 1);
-
 const fillInTheBlank = () => {
   const navigation = useNavigation();
+  const [currentSentence, setCurrentSentence] = useState<string>("");
+
+  const handleSubmit = () => {
+    console.log(currentSentence);
+  };
+
+  const handleSentenceChange = useCallback((sentence: string) => {
+    console.log("Working");
+    setCurrentSentence(sentence);
+  }, []);
 
   useFocusEffect(
     useCallback(() => {
       navigation.setOptions({
-        headerTitle: "Picture Flashcards",
+        headerTitle: "Fill in the Blank",
         headerStyle: {
           backgroundColor: "#2264DC",
         },
@@ -59,129 +63,88 @@ const fillInTheBlank = () => {
     }, [navigation])
   );
 
-  const initialDropZones: Dropzone[] = Array.from(
-    { length: DROP_ZONE_COUNT },
-    (_, index) => ({
-      id: index + 1,
-      x: DROP_ZONE_SPACING * (index + 1) + BOX_SIZE * index,
-      y: 300,
-      occupiedBy: null,
-    })
-  );
-
-  const initialDraggableBoxes: Box[] = [
-    {
-      id: 1,
-      x: DROP_ZONE_SPACING,
-      y: 100,
-      word: "hello",
-    },
-    {
-      id: 2,
-      x: DROP_ZONE_SPACING,
-      y: 100,
-      word: "hello",
-    },
-    {
-      id: 3,
-      x: DROP_ZONE_SPACING,
-      y: 100,
-      word: "hello",
-    },
-    {
-      id: 4,
-      x: DROP_ZONE_SPACING,
-      y: 100,
-      word: "hello",
-    },
-  ];
-
-  const [dropZones, setDropZones] = useState<Dropzone[]>(initialDropZones);
-  const [draggableBoxes, setDraggableBoxes] = useState<Box[]>(
-    initialDraggableBoxes
-  );
-  const [boxArrangement, setBoxArrangement] = useState<Box[]>([]);
-
-  const translateValues = draggableBoxes.map(() => ({
-    translateX: useSharedValue(0),
-    translateY: useSharedValue(0),
-  }));
-
   return (
-    <View style={styles.container}>
-      <ActivityProgress
-        difficulty="Easy"
-        totalItems={10}
-        completedItems={0}
-        instruction="Drag the word to complete the sentence"
-      />
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <View style={styles.progressContainer}>
+          <ActivityProgress
+            difficulty="Easy"
+            totalItems={10}
+            completedItems={0}
+            instruction="Drag the words to complete the sentence"
+          />
+        </View>
 
-      <View style={styles.cardContainer}>
-        <View style={{ flex: 1 }}>
-          <View style={styles.questionCard}>
-            <FontAwesome6
-              name="volume-high"
-              size={20}
-              color="#fff"
-              style={styles.speakerIcon}
-            />
-          </View>
-          <View style={{ backgroundColor: "transparent", flex: 1 }}>
-            <GestureHandlerRootView>
-              {draggableBoxes.map((box, index) => {
-                const animatedStyle = useAnimatedStyle(() => ({
-                  transform: [
-                    { translateX: translateValues[index].translateX.value },
-                    { translateY: translateValues[index].translateY.value },
-                  ],
-                }));
-
-                return (
-                  <PanGestureHandler>
-                    <Animated.View
-                      style={[{ left: box.x, top: box.y }, animatedStyle]}
-                    >
-                      <Word key={index} id={box.id} word={box.word}></Word>
-                    </Animated.View>
-                  </PanGestureHandler>
-                );
-              })}
-
-              {dropZones.map((zone) => (
-                <View key={zone.id}>
-                  <Word id={zone.id} word={zone.occupiedBy || ""} />
-                </View>
-              ))}
-            </GestureHandlerRootView>
+        <View style={styles.contentContainer}>
+          <View style={styles.sentenceContainer}>
+            <View style={styles.speakerIcon}>
+              <FontAwesome6 name="volume-high" size={20} color="#fff" />
+            </View>
+            <View style={styles.wordsArea}>
+              <WordList onSentenceChange={handleSentenceChange}>
+                {words.map((word) => (
+                  <Word key={word.id} {...word} />
+                ))}
+              </WordList>
+            </View>
           </View>
         </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </GestureHandlerRootView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#f5f5f5",
     padding: 20,
   },
-  cardContainer: {
-    height: "50%",
+  progressContainer: {},
+  contentContainer: {},
+  cardContainer: {},
+  sentenceContainer: {
+    backgroundColor: "white",
+    borderRadius: 15,
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+    height: "63%",
   },
-  questionCard: {
-    flexDirection: "column",
-    height: "70%",
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    padding: 17,
+  wordsArea: {
+    backgroundColor: "transparent",
+    paddingTop: 20,
+    height: 300,
   },
   speakerIcon: {
     backgroundColor: "#FFBF18",
     borderRadius: 180,
-    paddingHorizontal: 10,
-    paddingVertical: 12,
     alignSelf: "flex-start",
+    height: 50,
+    width: 50,
+    alignContent: "center",
+  },
+  button: {
+    backgroundColor: "#FFBF18",
+    padding: 15,
+    borderRadius: 50,
+    alignItems: "center",
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    justifyContent: "flex-end",
   },
 });
 
