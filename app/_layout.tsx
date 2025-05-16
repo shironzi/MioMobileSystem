7d6eefa1da8d32b6686cc4a07b0a973818939309
shironzi <157195223+shironzi@users.noreply.github.com>
2025-05-16 23:20:51 +0800

    Initial commit
    Generated by create-expo-app 3.4.2.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,78 +1,29 @@
-import { Stack, useNavigation, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
-import { verifyToken } from "@/utils/auth";
-import * as SecureStore from "expo-secure-store";
-import { StackActions } from "@react-navigation/native";
-import { Text, View } from "react-native";
-import { useAuthGuard } from "@/utils/useAuthGuard";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
 
-export default function Layout() {
-  const navigation = useNavigation();
-  const router = useRouter();
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [hasAuthError, setHasAuthError] = useState(false);
+import { useColorScheme } from '@/hooks/useColorScheme';
 
-  const rootNav = navigation.getParent();
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
 
-  useAuthGuard(hasAuthError);
-
-  useEffect(() => {
-    let redirected = false;
-
-    async function checkAuth() {
-      try {
-        const serverUserId = await verifyToken();
-        const raw = await SecureStore.getItemAsync("sessionData");
-
-        if (!raw) {
-          throw new Error("No session");
-        }
-
-        const { userId } = JSON.parse(raw);
-        if (userId !== serverUserId) {
-          // session mismatch → clear & redirect
-          await SecureStore.deleteItemAsync("sessionData");
-          redirected = true;
-          rootNav?.dispatch(StackActions.replace("/index"));
-          return;
-        }
-
-        // valid session
-        setIsLoggedIn(true);
-      } catch (err) {
-        console.error("Auth check failed:", err);
-        if (!redirected) {
-          setIsLoggedIn(false);
-          rootNav?.dispatch(StackActions.replace("/index"));
-        }
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    checkAuth();
-  }, []); // run once on mount
-
-  if (isLoggedIn === null) {
+  if (!loaded) {
+    // Async font loading only occurs in development.
     return null;
   }
 
-  if (loading) {
-    return (
-      <View>
-        <Text>Loading.......</Text>
-      </View>
-    );
-  }
-
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      {isLoggedIn ? (
-        <Stack.Screen name="(drawer)" />
-      ) : (
-        <Stack.Screen name="index" />
-      )}
-    </Stack>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
