89e9ddb2d73228301cdd49c318a4236b8a45f51a
shironzi <aaronbaon1@gmail.com>
2025-05-18 19:39:59 +0800

    feat: implement addAnnouncement component

---
+++ app/subject/(sub-details)/announcement/addAnnouncement.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const addAnnouncement = () => {
+  return (
+    <View>
+      <Text>addAnnouncement</Text>
+    </View>
+  );
+};
+
+export default addAnnouncement;
