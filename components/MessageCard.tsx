820e46191a98ff884bc2e49c78265c9e6a653844
notsoeyvah <avarce15@gmail.com>
2025-05-21 23:01:35 +0800

    fix mess card

--- components/MessageCard.tsx
+++ components/MessageCard.tsx
@@ -1,85 +1,92 @@
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useRouter } from "expo-router";
 import React, { memo, useCallback } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 const MessageCard = (props: {
-  title: string;
-  date: Date;
-  time: string;
-  desc: string;
-  type: string;
+	name: string;
+	date: Date;
+	time: string;
+	desc: string;
+	type: string;
 }) => {
-  const router = useRouter();
+	const router = useRouter();
 
-  const formatDate = useCallback(
-    (date: Date) => {
-      return date.toLocaleDateString("en-US", {
-        month: "short",
-        day: "2-digit",
-        year: "numeric",
-      });
-    },
-    [Date]
-  );
+	const formatDate = useCallback(
+		(date: Date) => {
+			return date.toLocaleDateString("en-US", {
+				month: "short",
+				day: "2-digit",
+				year: "numeric",
+			});
+		},
+		[Date]
+	);
 
-  return (
-    <TouchableOpacity
-      onPress={() => {
-        // router.push("messageDetails")
-        console.log("message Details");
-      }}
-    >
-      <View
-        style={{
-          padding: 0,
-          margin: 0,
-          backgroundColor: "#fff",
-        }}
-      >
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            overflow: "hidden",
-            height: 80,
-            marginTop: 15,
-          }}
-        >
-          <MaterialIcons
-            name="account-circle"
-            size={45}
-            color="black"
-            style={{ paddingHorizontal: 20, top: -5 }}
-          />
-          <View>
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
-            >
-              <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-                {props.title}
-              </Text>
-              <Text style={{ fontSize: 12, marginTop: 5, marginRight: 15 }}>
-                {formatDate(props.date)}
-              </Text>
-            </View>
-            <Text
-              style={{ fontSize: 14, marginTop: 5, height: 60, width: 300 }}
-            >
-              {props.desc.length > 100
-                ? props.desc.substring(0, 100 - 3) + "..."
-                : props.desc}
-            </Text>
-          </View>
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
+	return (
+		<TouchableOpacity
+			onPress={() => {
+				// router.push("messageDetails")
+				console.log("message Details");
+			}}
+		>
+			<View
+				style={{
+					padding: 0,
+					margin: 0,
+					backgroundColor: "#fff",
+					alignItems: "center",
+				}}
+			>
+				<View
+					style={{
+						flexDirection: "row",
+						alignItems: "center",
+						margin: 0,
+						marginBottom: -20,
+						top: -10,
+						padding: 10,
+						paddingBottom: -20,
+						justifyContent: "center",
+					}}
+				>
+					<Image
+						source={require("@/assets/1.png")}
+						style={{
+							width: 50,
+							height: 50,
+							borderRadius: 50,
+							margin: 20,
+							alignItems: "center",
+							top: -5,
+						}}
+					/>
+					<View>
+						<View
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "space-between",
+							}}
+						>
+							<Text style={{ fontSize: 18, fontWeight: 500 }}>
+								{props.name}
+							</Text>
+							<Text style={{ fontSize: 12, marginTop: 5, marginRight: 15 }}>
+								{formatDate(props.date)}
+							</Text>
+						</View>
+						<Text
+							style={{ fontSize: 14, marginTop: 5, height: 50, width: 300 }}
+						>
+							{props.desc.length > 100
+								? props.desc.substring(0, 50 - 3) + "..."
+								: props.desc}
+						</Text>
+					</View>
+				</View>
+			</View>
+		</TouchableOpacity>
+	);
 };
 
 export default memo(MessageCard);
