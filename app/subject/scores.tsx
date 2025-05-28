1b265861c9f302aa927f4f34612f1f47b539bba2
shironzi <aaronbaon1@gmail.com>
2025-05-29 00:57:34 +0800

    fix: fix dependencies

--- app/subject/scores.tsx
+++ app/subject/scores.tsx
@@ -60,7 +60,7 @@ const ScoresScreen = () => {
     return selectedCategory === "all"
       ? data
       : data.filter((item) => item.category === selectedCategory);
-  }, [data, selectedCategory]);
+  }, [selectedCategory]);
 
   const handleSelect = useCallback((value: scoreType | "all") => {
     setSelectedCategory(value);
