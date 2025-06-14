004185cf73f5a4b08abe696527336307ae568f1f
notsoeyvah <avarce15@gmail.com>
2025-06-15 02:07:36 +0800

    update UI for course details

--- app/subject/courseDetails.tsx
+++ app/subject/courseDetails.tsx
@@ -1,252 +1,271 @@
-import Entypo from "@expo/vector-icons/Entypo";
+import { FontAwesome6 } from "@expo/vector-icons";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import React, { memo, useCallback } from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+	Image,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from "react-native";
 
 import HeaderConfig from "@/utils/HeaderConfig";
 
-const courseDetails = () => {
-  const router = useRouter();
+const newCourseDetails = () => {
+	const router = useRouter();
+	const { id, description, title, role, subjectType, specializedType } =
+		useLocalSearchParams<{
+			id: string;
+			title: string;
+			description: string;
+			subjectType: string;
+			specializedType?: string;
+			role: string;
+		}>();
 
-  const { id, description, title, role, subjectType, specializedType } =
-    useLocalSearchParams<{
-      id: string;
-      title: string;
-      description: string;
-      subjectType: string;
-      specializedType?: string;
-      role: string;
-    }>();
+	const headerTitle =
+		subjectType === "specialized"
+			? specializedType === "speech"
+				? "Speech Development"
+				: specializedType === "auditory"
+					? "Auditory Development"
+					: specializedType === "language"
+						? "Language Development"
+						: "Course Details"
+			: "Course Details";
 
-  HeaderConfig("Course Details");
+	HeaderConfig(headerTitle);
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.courseInfoContainer}>
-        <View style={styles.courseInfo}></View>
-        <View>
-          <Text style={[styles.fontSizeTwo, { color: "#fff" }]}>{id}</Text>
-          <Text style={[styles.fontSizeOne, { color: "#fff" }]}>{title}</Text>
-          <Text style={[styles.fontSizeTwo, { color: "#fff" }]}>
-            {description}
-          </Text>
-        </View>
-      </View>
-      <View style={styles.linksContainer}>
-        {subjectType === "specialized" && (
-          <TouchableOpacity
-            style={styles.link}
-            onPress={useCallback(() => {
-              switch (specializedType) {
-                case "speech":
-                  router.push({
-                    pathname:
-                      "/subject/(exercises)/(speech)/speechTrainingExercises",
-                    params: { subjectId: id, role: role },
-                  });
-                  break;
-                case "auditory":
-                  router.push({
-                    pathname:
-                      "/subject/(exercises)/(auditory)/auditoryTrainingExercise",
-                    params: { subjectId: id, role: role },
-                  });
-                  break;
-                case "language":
-                  router.push({
-                    pathname:
-                      "/subject/(exercises)/(language)/languageTrainingExercises",
-                    params: { subjectId: id, role: role },
-                  });
-                  break;
-                default:
-                  router.push("/subject/courseDetails");
-              }
-            }, [router, specializedType, id, role])}
-          >
-            <View style={styles.linkContent}>
-              <Image
-                source={
-                  specializedType === "speech"
-                    ? require("@/assets/icons/speech.png")
-                    : specializedType === "auditory"
-                      ? require("@/assets/icons/auditory.png")
-                      : specializedType === "language"
-                        ? require("@/assets/icons/language.png")
-                        : null
-                }
-                style={{ width: 37, height: 37 }}
-              />
-              <View style={styles.linkTextContainer}>
-                <Text style={styles.fontSizeOne}>
-                  {specializedType} Training Exercises
-                </Text>
-                <Entypo name="chevron-small-right" size={30} color="#CCC" />
-              </View>
-            </View>
-          </TouchableOpacity>
-        )}
-        <TouchableOpacity
-          style={styles.link}
-          onPress={useCallback(
-            () =>
-              router.push({
-                pathname: "/subject/announcements",
-                params: { subjectId: id, role: role },
-              }),
-            [router, id, role],
-          )}
-        >
-          <View style={styles.yellowBulletin}></View>
-          <View style={styles.linkDecoration}>
-            <Text style={styles.fontSizeOne}>Announcements</Text>
-            <Entypo name="chevron-small-right" size={30} color="#CCC" />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.link}
-          onPress={useCallback(
-            () =>
-              router.push({
-                pathname: "/subject/assignments",
-                params: { subjectId: id, role: role },
-              }),
-            [router, id, role],
-          )}
-        >
-          <View style={styles.yellowBulletin}></View>
-          <View style={styles.linkDecoration}>
-            <Text style={styles.fontSizeOne}>Assignments</Text>
-            <Entypo name="chevron-small-right" size={30} color="#CCC" />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.link}
-          onPress={useCallback(
-            () =>
-              router.push({
-                pathname: "/subject/Scores",
-                params: { subjectId: id, role: role },
-              }),
-            [router, id, role],
-          )}
-        >
-          <View style={styles.yellowBulletin}></View>
-          <View style={styles.linkDecoration}>
-            <Text style={styles.fontSizeOne}>Scores</Text>
-            <Entypo name="chevron-small-right" size={30} color="#CCC" />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.link}
-          onPress={useCallback(
-            () =>
-              router.push({
-                pathname: "/subject/modules",
-                params: { subjectId: id },
-              }),
-            [router, id],
-          )}
-        >
-          <View style={styles.yellowBulletin}></View>
-          <View style={styles.linkDecoration}>
-            <Text style={styles.fontSizeOne}>Modules</Text>
-            <Entypo name="chevron-small-right" size={30} color="#CCC" />
-          </View>
-        </TouchableOpacity>
-        {role === "teacher" ? (
-          <TouchableOpacity
-            style={styles.link}
-            onPress={useCallback(
-              () => router.push("/subject/attendance"),
-              [router],
-            )}
-          >
-            <View style={styles.yellowBulletin}></View>
-            <View style={styles.linkDecoration}>
-              <Text style={styles.fontSizeOne}>Attendance</Text>
-              <Entypo name="chevron-small-right" size={30} color="#CCC" />
-            </View>
-          </TouchableOpacity>
-        ) : null}
-      </View>
-    </View>
-  );
+	return (
+		<ScrollView
+			showsVerticalScrollIndicator={false}
+			contentContainerStyle={{ paddingBottom: 50 }}
+			style={{ backgroundColor: "#fff", flex: 1 }}
+		>
+			<View style={styles.cardContainer}>
+				<Image
+					source={require("@/assets/dashImage/english.png")}
+					style={styles.subImg}
+				/>
+				<View style={styles.group}>
+					<Text style={styles.subId}>{id}</Text>
+					<Text style={styles.subName}>{title}</Text>
+					<Text style={styles.subDesc}>{description}</Text>
+				</View>
+			</View>
+
+			<View style={styles.subContainer}>
+				{subjectType === "specialized" && (
+					<TouchableOpacity
+						onPress={useCallback(() => {
+							switch (specializedType) {
+								case "speech":
+									router.push({
+										pathname:
+											"/subject/(exercises)/(speech)/speechTrainingExercises",
+										params: { subjectId: id, role },
+									});
+									break;
+								case "auditory":
+									router.push({
+										pathname:
+											"/subject/(exercises)/(auditory)/auditoryTrainingExercise",
+										params: { subjectId: id, role },
+									});
+									break;
+								case "language":
+									router.push({
+										pathname:
+											"/subject/(exercises)/(language)/languageTrainingExercises",
+										params: { subjectId: id, role },
+									});
+									break;
+								default:
+									router.push("/subject/courseDetails");
+							}
+						}, [router, specializedType, id, role])}
+					>
+						<Image
+							source={
+								specializedType === "speech"
+									? require("@/assets/course/speech.png")
+									: specializedType === "auditory"
+										? require("@/assets/course/auditory.png")
+										: require("@/assets/course/language.png")
+							}
+							style={styles.speech}
+						/>
+					</TouchableOpacity>
+				)}
+
+				<TouchableOpacity
+					style={styles.subCourse}
+					onPress={useCallback(() => {
+						router.push({
+							pathname: "/subject/announcements",
+							params: { subjectId: id, role },
+						});
+					}, [router, id, role])}
+				>
+					<View style={styles.row}>
+						<Image
+							source={require("@/assets/course/ann.png")}
+							style={styles.courseImg}
+						/>
+						<Text style={{ fontSize: 14, left: -40 }}>Announcements</Text>
+						<FontAwesome6 name="arrow-right-long" size={20} color="#1f1f1f" />
+					</View>
+				</TouchableOpacity>
+
+				<TouchableOpacity
+					style={styles.subCourse}
+					onPress={useCallback(() => {
+						router.push({
+							pathname: "/subject/assignments",
+							params: { subjectId: id, role },
+						});
+					}, [router, id, role])}
+				>
+					<View style={styles.row}>
+						<Image
+							source={require("@/assets/course/ass.png")}
+							style={styles.courseImg}
+						/>
+						<Text style={{ fontSize: 14, left: -50 }}>Assignments</Text>
+						<FontAwesome6 name="arrow-right-long" size={20} color="#1f1f1f" />
+					</View>
+				</TouchableOpacity>
+
+				<TouchableOpacity
+					style={styles.subCourse}
+					onPress={useCallback(() => {
+						router.push({
+							pathname: "/subject/Scores",
+							params: { subjectId: id, role },
+						});
+					}, [router, id, role])}
+				>
+					<View style={styles.row}>
+						<Image
+							source={require("@/assets/course/score.png")}
+							style={styles.courseImg}
+						/>
+						<Text style={{ fontSize: 14, left: -70 }}>Scores</Text>
+						<FontAwesome6 name="arrow-right-long" size={20} color="#1f1f1f" />
+					</View>
+				</TouchableOpacity>
+
+				<TouchableOpacity
+					style={styles.subCourse}
+					onPress={useCallback(() => {
+						router.push({
+							pathname: "/subject/modules",
+							params: { subjectId: id },
+						});
+					}, [router, id])}
+				>
+					<View style={styles.row}>
+						<Image
+							source={require("@/assets/course/module.png")}
+							style={[styles.courseImg, { width: 48, left: -10 }]}
+						/>
+						<Text style={{ fontSize: 14, left: -65 }}>Modules</Text>
+						<FontAwesome6 name="arrow-right-long" size={20} color="#1f1f1f" />
+					</View>
+				</TouchableOpacity>
+
+				{role === "teacher" && (
+					<TouchableOpacity
+						style={styles.subCourse}
+						onPress={useCallback(() => {
+							router.push("/subject/attendance");
+						}, [router])}
+					>
+						<View style={styles.row}>
+							<Image
+								source={require("@/assets/course/attendance.png")}
+								style={[styles.courseImg, { width: 40 }]}
+							/>
+							<Text style={{ fontSize: 14, left: -50 }}>Attendance</Text>
+							<FontAwesome6 name="arrow-right-long" size={20} color="#1f1f1f" />
+						</View>
+					</TouchableOpacity>
+				)}
+			</View>
+		</ScrollView>
+	);
 };
 
-const styles = StyleSheet.create({
-  container: {
-    margin: 15,
-  },
-  courseInfoContainer: {
-    backgroundColor: "#1F1F1F",
-    paddingLeft: 13,
-    paddingRight: 50,
-    paddingVertical: 15,
-    elevation: 5,
-    display: "flex",
-    flexDirection: "row",
-    columnGap: 14,
-    borderRadius: 10,
-  },
-  courseInfo: {
-    borderColor: "#fff",
-    borderWidth: 2.5,
-    borderRadius: 100,
-  },
-  linksContainer: {
-    rowGap: 10,
-    marginTop: 10,
-  },
-  link: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    columnGap: 11,
-    backgroundColor: "#fff",
-    paddingVertical: 12,
-    paddingHorizontal: 12,
-    borderRadius: 10,
-    elevation: 2,
-    margin: 3,
-  },
-  linkContent: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    columnGap: 12,
-    backgroundColor: "#fff",
-    paddingVertical: 4,
-    borderRadius: 10,
-    width: "100%",
-  },
-  linkTextContainer: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  fontSizeOne: {
-    fontSize: 18,
-    textAlignVertical: "center",
-  },
-  fontSizeTwo: {
-    fontSize: 12,
-  },
-  linkDecoration: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "100%",
-    paddingRight: 18,
-  },
-  yellowBulletin: {
-    borderColor: "#FFBF18",
-    backgroundColor: "#FFBF18",
-    height: "100%",
-    borderWidth: 2.5,
-    borderRadius: 100,
-  },
-});
+export default memo(newCourseDetails);
 
-export default memo(courseDetails);
+const styles = StyleSheet.create({
+	cardContainer: {
+		margin: 20,
+		// padding: 20,
+		// backgroundColor: "#000",
+		borderColor: "#ddd",
+		borderWidth: 1,
+		borderRadius: 20,
+		// borderTopLeftRadius: 20,
+		// borderTopRightRadius: 20,
+	},
+	subImg: {
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+		height: 130,
+		width: 351,
+	},
+	group: {
+		margin: 20,
+	},
+	subId: {
+		color: "#828282",
+		top: -10,
+		fontSize: 14,
+		// margin: 10,
+		// left: 10,
+	},
+	subName: {
+		top: -5,
+		fontSize: 18,
+		color: "#2264dc",
+		fontWeight: 500,
+		// margin: 10,
+		// left: 10,
+	},
+
+	subDesc: {
+		textAlign: "justify",
+		fontWeight: 300,
+		fontSize: 12,
+	},
+	subContainer: {},
+	speech: {
+		height: 110,
+		width: 355,
+		left: 20,
+		marginVertical: -10,
+		marginBottom: 5,
+	},
+	subCourse: {
+		backgroundColor: "000",
+		borderColor: "#ddd",
+		borderWidth: 1,
+		marginHorizontal: 20,
+		marginVertical: 10,
+		borderRadius: 20,
+		padding: 10,
+	},
+	courseImg: {
+		height: 40,
+		width: 40,
+		left: -10,
+	},
+	row: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		paddingHorizontal: 30,
+	},
+});
