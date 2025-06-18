bbfe195f6c9eab96201808dde851b1a667b6d75c
notsoeyvah <avarce15@gmail.com>
2025-06-18 17:34:25 +0800

    Update play UI

--- app/subject/(exercises)/play.tsx
+++ app/subject/(exercises)/play.tsx
@@ -1,126 +1,250 @@
-import HeaderConfig from "@/utils/HeaderConfig";
+import LoadingCard from "@/components/loadingCard";
 import PlayCard from "@/components/playCard";
+import globalStyles from "@/styles/globalStyles";
+import HeaderConfig from "@/utils/HeaderConfig";
+import { getSpecializedActivities } from "@/utils/specialized";
+import { FontAwesome, Fontisto, Ionicons } from "@expo/vector-icons";
+import { useLocalSearchParams } from "expo-router";
 import React, { memo, useEffect, useState } from "react";
 import {
-  ActivityIndicator,
-  Alert,
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
+	Alert,
+	FlatList,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View,
 } from "react-native";
-import globalStyles from "@/styles/globalStyles";
-import { useLocalSearchParams } from "expo-router";
-import { getSpecializedActivities } from "@/utils/specialized";
 
 const Play = () => {
-  HeaderConfig("Play");
+	HeaderConfig("Play");
 
-  const { activity_type, difficulty, category, subjectId, role } =
-    useLocalSearchParams<{
-      subjectId: string;
-      activity_type: string;
-      difficulty: string;
-      category: string;
-      role: string;
-    }>();
+	const { activity_type, difficulty, category, subjectId, role } =
+		useLocalSearchParams<{
+			subjectId: string;
+			activity_type: string;
+			difficulty: string;
+			category: string;
+			role: string;
+		}>();
 
-  const [activities, setActivities] = useState<string[]>();
-  const [loading, setLoading] = useState(true);
+	const [activities, setActivities] = useState<string[]>();
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchActivities = async () => {
-      try {
-        const res = await getSpecializedActivities(
-          subjectId,
-          activity_type,
-          difficulty,
-        );
+	useEffect(() => {
+		const fetchActivities = async () => {
+			try {
+				const res = await getSpecializedActivities(
+					subjectId,
+					activity_type,
+					difficulty
+				);
 
-        setActivities(res.activities);
-        setLoading(false);
-      } catch (err) {
-        console.error("Get Activities Fetch Failed: " + err);
-        Alert.alert("Error", "Failed to load activities.");
-      }
-    };
+				setActivities(res.activities);
+				setLoading(false);
+			} catch (err) {
+				console.error("Get Activities Fetch Failed: " + err);
+				Alert.alert("Error", "Failed to load activities.");
+			}
+		};
 
-    fetchActivities();
-  }, []);
+		fetchActivities();
+	}, []);
 
-  if (loading) {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#fff",
-        }}
-      >
-        <ActivityIndicator size="large" color="#007bff" />
-        <Text style={{ marginTop: 10, fontSize: 16, color: "#333" }}>
-          Loading...
-        </Text>
-      </View>
-    );
-  }
+	if (loading) {
+		return (
+			<View
+				style={{
+					flex: 1,
+					justifyContent: "center",
+					alignItems: "center",
+					backgroundColor: "#fff",
+				}}
+			>
+				<LoadingCard></LoadingCard>
+			</View>
+		);
+	}
 
-  if (!activities || activities.length === 0) {
-    return (
-      <View style={globalStyles.container}>
-        <Text>No activities found.</Text>
-      </View>
-    );
-  }
+	if (!activities || activities.length === 0) {
+		return (
+			<View style={globalStyles.container}>
+				<Text>No activities found.</Text>
+			</View>
+		);
+	}
 
-  const renderItem = (item: { item: string; index: number }) => (
-    <PlayCard
-      id={item.index}
-      label="Play"
-      activity_type={activity_type}
-      difficulty={difficulty}
-      category={category}
-      subjectId={subjectId}
-      activityId={item.item}
-      role={role}
-    />
-  );
+	const difficultyStyles: Record<
+		string,
+		{ backgroundColor: string; borderColor: string }
+	> = {
+		easy: { backgroundColor: "#C8FFB7", borderColor: "#439558" },
+		Average: { backgroundColor: "#ffe9ae", borderColor: "#ffbf18" },
+		Difficult: { backgroundColor: "#FFCEA1", borderColor: "#FF7A00" },
+		Challenge: { backgroundColor: "#FFB1B1", borderColor: "#DB4141" },
+	};
 
-  return (
-    <View style={globalStyles.container}>
-      <Text style={styles.title}>{difficulty}</Text>
-      <FlatList
-        data={activities}
-        renderItem={renderItem}
-        keyExtractor={(item) => item}
-        numColumns={3}
-        columnWrapperStyle={[
-          styles.row,
-          activities.length >= 3
-            ? { justifyContent: "space-between" }
-            : { columnGap: 20 },
-        ]}
-        contentContainerStyle={styles.listContainer}
-        style={{ paddingHorizontal: 5 }}
-      />
-    </View>
-  );
+	const renderHeaderCard = () => {
+		switch (difficulty?.toLowerCase()) {
+			case "easy":
+				return (
+					<View style={[styles.subLevel, difficultyStyles.easy]}>
+						<Fontisto
+							name="star"
+							size={40}
+							color="#439558"
+							style={styles.shape1}
+						/>
+						<Text style={styles.try}>TRY THE</Text>
+						<Text style={styles.name}>Easy Mode</Text>
+						<Fontisto
+							name="star"
+							size={70}
+							color="#439558"
+							style={styles.shape2}
+						/>
+					</View>
+				);
+			case "average":
+				return (
+					<View style={[styles.subLevel, difficultyStyles.Average]}>
+						<Ionicons
+							name="square"
+							size={40}
+							color="#ffbf18"
+							style={styles.shape1}
+						/>
+						<Text style={styles.try}>TRY THE</Text>
+						<Text style={styles.name}>Average Mode</Text>
+						<Ionicons
+							name="square"
+							size={70}
+							color="#ffbf18"
+							style={styles.shape2}
+						/>
+					</View>
+				);
+			case "difficult":
+				return (
+					<View style={[styles.subLevel, difficultyStyles.Difficult]}>
+						<Ionicons
+							name="triangle"
+							size={40}
+							color="#FF7A00"
+							style={styles.shape1}
+						/>
+						<Text style={styles.try}>TRY THE</Text>
+						<Text style={styles.name}>Difficult Mode</Text>
+						<Ionicons
+							name="triangle"
+							size={70}
+							color="#FF7A00"
+							style={styles.shape2}
+						/>
+					</View>
+				);
+			case "challenge":
+				return (
+					<View style={[styles.subLevel, difficultyStyles.Challenge]}>
+						<FontAwesome
+							name="circle"
+							size={40}
+							color="#DB4141"
+							style={styles.shape1}
+						/>
+						<Text style={styles.try}>TRY THE</Text>
+						<Text style={styles.name}>Challenge Mode</Text>
+						<FontAwesome
+							name="circle"
+							size={70}
+							color="#DB4141"
+							style={styles.shape2}
+						/>
+					</View>
+				);
+			default:
+				return null;
+		}
+	};
+
+	const renderItem = (item: { item: string; index: number }) => (
+		<PlayCard
+			id={item.index}
+			label="Play"
+			activity_type={activity_type}
+			difficulty={difficulty}
+			category={category}
+			subjectId={subjectId}
+			activityId={item.item}
+			role={role}
+		/>
+	);
+
+	return (
+		<ScrollView
+			showsVerticalScrollIndicator={false}
+			contentContainerStyle={{ paddingBottom: 50 }}
+			style={[
+				globalStyles.container,
+				{ marginHorizontal: -20, marginTop: -20 },
+			]}
+		>
+			{renderHeaderCard()}
+			{/* <Text style={styles.title}>{difficulty}</Text> */}
+			<FlatList
+				data={activities}
+				renderItem={renderItem}
+				keyExtractor={(item) => item}
+				contentContainerStyle={styles.listContainer}
+				style={{ paddingHorizontal: 5 }}
+			/>
+		</ScrollView>
+	);
 };
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#000",
-    marginBottom: 20,
-  },
-  listContainer: {
-    paddingBottom: 20,
-  },
-  row: {
-    marginBottom: 15,
-  },
+	title: {
+		fontSize: 24,
+		fontWeight: "bold",
+		color: "#000",
+		marginBottom: 20,
+	},
+	listContainer: {
+		paddingBottom: 20,
+	},
+	row: {
+		marginBottom: 15,
+	},
+	shape1: {
+		transform: [{ rotate: "-15deg" }],
+		left: 20,
+		top: -15,
+	},
+	shape2: {
+		position: "absolute",
+		transform: [{ rotate: "20deg" }],
+		left: 250,
+		top: 45,
+	},
+	try: {
+		fontSize: 12,
+		fontWeight: 400,
+		left: 90,
+		top: -12,
+	},
+	name: {
+		fontSize: 16,
+		fontWeight: 500,
+		left: 90,
+		top: -10,
+	},
+	subLevel: {
+		margin: 20,
+		borderWidth: 1,
+		borderRadius: 20,
+		height: 100,
+		overflow: "hidden",
+		position: "relative",
+	},
 });
 
 export default memo(Play);
--- components/playCard.tsx
+++ components/playCard.tsx
@@ -1,122 +1,149 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import React, { memo } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+const difficultyColors: Record<string, string> = {
+	easy: "#439558",
+	Average: "#ffbf18",
+	Difficult: "#FF7A00",
+	Challenge: "#DB4141",
+};
+
 const PlayCard = (props: {
-  id: number;
-  label: string;
-  activity_type: string;
-  difficulty: string;
-  category: string;
-  subjectId: string;
-  activityId: string;
-  role: string;
+	id: number;
+	label: string;
+	activity_type: string;
+	difficulty: string;
+	category: string;
+	subjectId: string;
+	activityId: string;
+	role: string;
 }) => {
-  const router = useRouter();
+	const router = useRouter();
 
-  const handleCategory = () => {
-    if (
-      props.activity_type === "picture" ||
-      props.activity_type === "pronunciation" ||
-      props.activity_type === "phrase" ||
-      props.activity_type === "question"
-    ) {
-      router.push({
-        pathname:
-          props.role === "teacher"
-            ? "/subject/(exercises)/(speech)/ManageActivity/AddSpeechActivity"
-            : "/subject/(exercises)/ViewActivity",
-        params: {
-          subjectId: props.subjectId,
-          activity_type: props.activity_type,
-          difficulty: props.difficulty,
-          category: props.category,
-          activityId: props.activityId,
-        },
-      });
-    } else if (
-      props.activity_type === "bingo" ||
-      props.activity_type === "matching"
-    ) {
-      router.push({
-        pathname:
-          props.role === "teacher"
-            ? "/subject/(exercises)/(auditory)/ManageAuditoryActivity/AddAuditoryActivity"
-            : "/subject/(exercises)/ViewActivity",
-        params: {
-          subjectId: props.subjectId,
-          activity_type: props.activity_type,
-          difficulty: props.difficulty,
-          category: props.category,
-          activityId: props.activityId,
-        },
-      });
-    } else if (
-      props.activity_type === "fill" ||
-      props.activity_type === "homonyms"
-    ) {
-      router.push({
-        pathname:
-          props.role === "teacher"
-            ? "/subject/(exercises)/(language)/ManageActivity/AddLanguageActivity"
-            : "/subject/(exercises)/ViewActivity",
-        params: {
-          subjectId: props.subjectId,
-          activity_type: props.activity_type,
-          difficulty: props.difficulty,
-          category: props.category,
-          activityId: props.activityId,
-        },
-      });
-    }
-  };
+	const handleCategory = () => {
+		if (
+			props.activity_type === "picture" ||
+			props.activity_type === "pronunciation" ||
+			props.activity_type === "phrase" ||
+			props.activity_type === "question"
+		) {
+			router.push({
+				pathname:
+					props.role === "teacher"
+						? "/subject/(exercises)/(speech)/ManageActivity/AddSpeechActivity"
+						: "/subject/(exercises)/ViewActivity",
+				params: {
+					subjectId: props.subjectId,
+					activity_type: props.activity_type,
+					difficulty: props.difficulty,
+					category: props.category,
+					activityId: props.activityId,
+				},
+			});
+		} else if (
+			props.activity_type === "bingo" ||
+			props.activity_type === "matching"
+		) {
+			router.push({
+				pathname:
+					props.role === "teacher"
+						? "/subject/(exercises)/(auditory)/ManageAuditoryActivity/AddAuditoryActivity"
+						: "/subject/(exercises)/ViewActivity",
+				params: {
+					subjectId: props.subjectId,
+					activity_type: props.activity_type,
+					difficulty: props.difficulty,
+					category: props.category,
+					activityId: props.activityId,
+				},
+			});
+		} else if (
+			props.activity_type === "fill" ||
+			props.activity_type === "homonyms"
+		) {
+			router.push({
+				pathname:
+					props.role === "teacher"
+						? "/subject/(exercises)/(language)/ManageActivity/AddLanguageActivity"
+						: "/subject/(exercises)/ViewActivity",
+				params: {
+					subjectId: props.subjectId,
+					activity_type: props.activity_type,
+					difficulty: props.difficulty,
+					category: props.category,
+					activityId: props.activityId,
+				},
+			});
+		}
+	};
 
-  return (
-    <TouchableOpacity style={styles.card} onPress={handleCategory}>
-      <View style={styles.borderLeft}></View>
-      <View style={styles.linkDecoration}>
-        <Text style={styles.cardNumber}>{props.id + 1}</Text>
-        <Text style={styles.cardWord}>{props.label}</Text>
-      </View>
-    </TouchableOpacity>
-  );
+	const difficultyColor =
+		difficultyColors[props.difficulty.toLowerCase()] || "#aaa";
+
+	return (
+		<TouchableOpacity style={styles.card} onPress={handleCategory}>
+			<View
+				style={{
+					flexDirection: "row",
+					justifyContent: "space-around",
+					top: 25,
+					left: -30,
+				}}
+			>
+				<FontAwesome
+					name="circle"
+					size={13}
+					color={difficultyColor}
+					style={styles.icon}
+				/>
+				<Text style={styles.cardNumber}>Exercise {props.id + 1}</Text>
+			</View>
+			<View style={[styles.play, { backgroundColor: difficultyColor }]}>
+				<Text
+					style={{
+						color: "#fff",
+						alignSelf: "center",
+						top: 6,
+						fontWeight: "300",
+						fontSize: 12,
+					}}
+				>
+					Play!
+				</Text>
+			</View>
+		</TouchableOpacity>
+	);
 };
 
 const styles = StyleSheet.create({
-  card: {
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    padding: 15,
-    width: "30%",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 5,
-    flexDirection: "row",
-  },
-  cardNumber: {
-    fontSize: 18,
-    color: "#000",
-  },
-  cardWord: {
-    fontSize: 18,
-    color: "#FFBF18",
-    textDecorationLine: "underline",
-  },
-  linkDecoration: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    width: "100%",
-  },
-  borderLeft: {
-    borderColor: "#FFBF18",
-    borderWidth: 2.5,
-    backgroundColor: "#FFBF18",
-    borderRadius: 100,
-    height: "100%",
-  },
+	card: {
+		borderColor: "#ddd",
+		borderWidth: 1,
+		borderRadius: 20,
+		marginHorizontal: 20,
+		marginVertical: 10,
+		top: -10,
+		height: 70,
+	},
+	icon: {
+		alignSelf: "center",
+	},
+	play: {
+		height: 30,
+		width: "30%",
+		alignSelf: "flex-end",
+		borderTopLeftRadius: 20,
+		borderBottomRightRadius: 20,
+		marginTop: 18,
+	},
+	cardNumber: {
+		fontSize: 14,
+		color: "#000",
+		left: -100,
+		fontWeight: "400",
+	},
 });
 
 export default memo(PlayCard);
