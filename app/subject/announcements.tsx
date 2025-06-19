d7b78283017903a0f1a9aa5a45fdd23a29c3c5e5
notsoeyvah <avarce15@gmail.com>
2025-06-20 03:49:14 +0800

    rm card for ann

--- app/subject/announcements.tsx
+++ app/subject/announcements.tsx
@@ -1,165 +1,178 @@
 import AnnounceCard from "@/components/AnnounceCard";
+import LoadingCard from "@/components/loadingCard";
+
+import ConfirmationModal from "@/components/ConfirmationModal";
 import HeaderConfig from "@/utils/HeaderConfig";
 import { deleteAnnouncements, getAnnouncements } from "@/utils/query";
 import { useAuthGuard } from "@/utils/useAuthGuard";
+import MaterialIcon from "@expo/vector-icons/MaterialIcons";
 import { useFocusEffect, useLocalSearchParams, useRouter } from "expo-router";
 import React, { memo, useCallback, useState } from "react";
 import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
 } from "react-native";
-import MaterialIcon from "@expo/vector-icons/MaterialIcons";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import ConfirmationModal from "@/components/ConfirmationModal";
 
 type Announcement = {
-  title: string;
-  description: string;
-  subject_id: string;
-  date_posted: string;
-  announcement_id: string;
+	title: string;
+	description: string;
+	subject_id: string;
+	date_posted: string;
+	announcement_id: string;
 };
 
 function Announcements() {
-  HeaderConfig("Announcements");
-  const router = useRouter();
+	HeaderConfig("Announcements");
+	const router = useRouter();
 
-  const { subjectId, role } = useLocalSearchParams<{
-    subjectId: string;
-    role: string;
-  }>();
+	const { subjectId, role } = useLocalSearchParams<{
+		subjectId: string;
+		role: string;
+	}>();
 
-  const [announcements, setAnnouncements] = useState<Announcement[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [deleteConfirm, setDeleteConfirm] = useState<boolean>(false);
-  const [targetAnnouncement, setTargetAnnouncement] = useState<string | null>(
-    null,
-  );
+	const [announcements, setAnnouncements] = useState<Announcement[]>([]);
+	const [loading, setLoading] = useState(true);
+	const [deleteConfirm, setDeleteConfirm] = useState<boolean>(false);
+	const [targetAnnouncement, setTargetAnnouncement] = useState<string | null>(
+		null
+	);
 
-  useFocusEffect(
-    useCallback(() => {
-      let isActive = true;
+	useFocusEffect(
+		useCallback(() => {
+			let isActive = true;
 
-      const fetchAnnouncements = async () => {
-        try {
-          const response = await getAnnouncements(subjectId);
-          if (isActive) {
-            setAnnouncements(response.announcements);
-          }
-        } catch (err) {
-          useAuthGuard(err);
-        } finally {
-          if (isActive) {
-            setLoading(false);
-          }
-        }
-      };
+			const fetchAnnouncements = async () => {
+				try {
+					const response = await getAnnouncements(subjectId);
+					if (isActive) {
+						setAnnouncements(response.announcements);
+					}
+				} catch (err) {
+					useAuthGuard(err);
+				} finally {
+					if (isActive) {
+						setLoading(false);
+					}
+				}
+			};
 
-      fetchAnnouncements();
+			fetchAnnouncements();
 
-      return () => {
-        isActive = false;
-      };
-    }, [subjectId]),
-  );
+			return () => {
+				isActive = false;
+			};
+		}, [subjectId])
+	);
 
-  const handleAdd = () => {
-    router.push({
-      pathname: "/subject/(sub-details)/announcement/addAnnouncement",
-      params: { subjectId },
-    });
-  };
+	const handleAdd = () => {
+		router.push({
+			pathname: "/subject/(sub-details)/announcement/addAnnouncement",
+			params: { subjectId },
+		});
+	};
 
-  if (loading) {
-    return (
-      <View>
-        <Text>Loading........</Text>
-      </View>
-    );
-  }
+	if (loading) {
+		return (
+			<View
+				style={{
+					flex: 1,
+					justifyContent: "center",
+					alignItems: "center",
+					backgroundColor: "#fff",
+				}}
+			>
+				<LoadingCard></LoadingCard>
+			</View>
+		);
+	}
 
-  const handleDelete = async () => {
-    if (targetAnnouncement === null) return;
+	const handleDelete = async () => {
+		if (targetAnnouncement === null) return;
 
-    try {
-      const res = await deleteAnnouncements(subjectId, targetAnnouncement);
+		try {
+			const res = await deleteAnnouncements(subjectId, targetAnnouncement);
 
-      console.log(res);
-      setDeleteConfirm(false);
-      setTargetAnnouncement(null);
-    } catch (err) {
-      console.error("Deleting error: " + err);
-    }
-  };
+			console.log(res);
+			setDeleteConfirm(false);
+			setTargetAnnouncement(null);
+		} catch (err) {
+			console.error("Deleting error: " + err);
+		}
+	};
 
-  return (
-    <View style={styles.container}>
-      <ScrollView>
-        {announcements.length > 0 ? (
-          <View style={{ rowGap: 15 }}>
-            {announcements.map((item, index) => (
-              <GestureHandlerRootView key={index}>
-                <AnnounceCard
-                  subjectId={subjectId}
-                  title={item.title}
-                  date={item.date_posted}
-                  time="09:00 AM"
-                  description={item.description}
-                  announcementId={item.announcement_id}
-                  role={role}
-                  handleDelete={() => {
-                    setDeleteConfirm(true);
-                    setTargetAnnouncement(item.announcement_id);
-                  }}
-                />
-              </GestureHandlerRootView>
-            ))}
-          </View>
-        ) : (
-          <View>
-            <Text>This subject has no announcements yet.</Text>
-          </View>
-        )}
-      </ScrollView>
+	return (
+		<View style={styles.container}>
+			<ScrollView
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={{ paddingBottom: 50 }}
+			>
+				{announcements.length > 0 ? (
+					<View style={{ rowGap: 15 }}>
+						{announcements.map((item, index) => (
+							<GestureHandlerRootView key={index}>
+								<AnnounceCard
+									subjectId={subjectId}
+									title={item.title}
+									date={item.date_posted}
+									time="09:00 AM"
+									description={item.description}
+									announcementId={item.announcement_id}
+									role={role}
+									handleDelete={() => {
+										setDeleteConfirm(true);
+										setTargetAnnouncement(item.announcement_id);
+									}}
+								/>
+							</GestureHandlerRootView>
+						))}
+					</View>
+				) : (
+					<View>
+						<Text>This subject has no announcements yet.</Text>
+					</View>
+				)}
+			</ScrollView>
 
-      <ConfirmationModal
-        isVisible={deleteConfirm}
-        description={"Are you sure you want to delete this announcement?"}
-        cancelDisplay={"Cancel"}
-        approveDisplay={"Delete"}
-        handleCancel={() => setDeleteConfirm(false)}
-        handleApprove={() => handleDelete()}
-      />
+			<ConfirmationModal
+				isVisible={deleteConfirm}
+				description={"Are you sure you want to delete this announcement?"}
+				cancelDisplay={"Cancel"}
+				approveDisplay={"Delete"}
+				handleCancel={() => setDeleteConfirm(false)}
+				handleApprove={() => handleDelete()}
+			/>
 
-      {role === "teacher" && (
-        <TouchableOpacity style={styles.addButton} onPress={handleAdd}>
-          <MaterialIcon name="add" size={30} color="#fff" />
-        </TouchableOpacity>
-      )}
-    </View>
-  );
+			{role === "teacher" && (
+				<TouchableOpacity style={styles.addButton} onPress={handleAdd}>
+					<MaterialIcon name="add" size={30} color="#fff" />
+				</TouchableOpacity>
+			)}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingVertical: 20,
-  },
-  addButton: {
-    position: "absolute",
-    bottom: 20,
-    right: 20,
-    backgroundColor: "#2264DC",
-    height: 60,
-    width: 60,
-    borderRadius: 30,
-    justifyContent: "center",
-    alignItems: "center",
-    elevation: 5,
-  },
+	container: {
+		flex: 1,
+		paddingVertical: 20,
+		backgroundColor: "#fff",
+	},
+	addButton: {
+		position: "absolute",
+		bottom: 20,
+		right: 20,
+		backgroundColor: "#2264DC",
+		height: 60,
+		width: 60,
+		borderRadius: 30,
+		justifyContent: "center",
+		alignItems: "center",
+		elevation: 5,
+	},
 });
 
 export default memo(Announcements);
--- components/AnnounceCard.tsx
+++ components/AnnounceCard.tsx
@@ -1,161 +1,165 @@
-import Entypo from "@expo/vector-icons/Entypo";
+import { FontAwesome6 } from "@expo/vector-icons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useRouter } from "expo-router";
 import React, { memo } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
-  runOnJS,
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
+	runOnJS,
+	useAnimatedStyle,
+	useSharedValue,
+	withTiming,
 } from "react-native-reanimated";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 const announceCard = (props: {
-  subjectId: string;
-  title: string;
-  date: string;
-  time: string;
-  description: string;
-  announcementId: string;
-  role: string;
-  handleDelete: () => void;
+	subjectId: string;
+	title: string;
+	date: string;
+	time: string;
+	description: string;
+	announcementId: string;
+	role: string;
+	handleDelete: () => void;
 }) => {
-  const router = useRouter();
+	const router = useRouter();
 
-  const newDate = new Date(props.date).toLocaleDateString("en-US", {
-    month: "short",
-    day: "2-digit",
-    year: "numeric",
-  });
+	const newDate = new Date(props.date).toLocaleDateString("en-US", {
+		month: "short",
+		day: "2-digit",
+		year: "numeric",
+	});
 
-  const handleNavigate = () => {
-    if (props.role === "teacher") {
-      router.push({
-        pathname: "/subject/(sub-details)/announcement/addAnnouncement",
-        params: {
-          subjectId: props.subjectId,
-          announcementId: props.announcementId,
-        },
-      });
-    } else {
-      router.push({
-        pathname: "/subject/(sub-details)/announcement/announcementDetails",
-        params: {
-          subjectId: props.subjectId,
-          title: props.title,
-          date: newDate,
-          time: props.time,
-          description: props.description,
-          announcementId: props.announcementId,
-          role: props.role,
-        },
-      });
-    }
-  };
+	const handleNavigate = () => {
+		if (props.role === "teacher") {
+			router.push({
+				pathname: "/subject/(sub-details)/announcement/addAnnouncement",
+				params: {
+					subjectId: props.subjectId,
+					announcementId: props.announcementId,
+				},
+			});
+		} else {
+			router.push({
+				pathname: "/subject/(sub-details)/announcement/announcementDetails",
+				params: {
+					subjectId: props.subjectId,
+					title: props.title,
+					date: newDate,
+					time: props.time,
+					description: props.description,
+					announcementId: props.announcementId,
+					role: props.role,
+				},
+			});
+		}
+	};
 
-  const translatedX = useSharedValue(0);
+	const translatedX = useSharedValue(0);
 
-  const panGesture = Gesture.Pan()
-    .onUpdate((e) => {
-      if (e.translationX < 0 && e.translationX > -110) {
-        translatedX.value = e.translationX;
-      }
-    })
-    .onEnd(() => {
-      if (translatedX.value < -90) {
-        translatedX.value = withTiming(-1000, { duration: 1500 });
-        runOnJS(props.handleDelete)();
-      }
-      translatedX.value = withTiming(0, { duration: 700 });
-    });
+	const panGesture = Gesture.Pan()
+		.onUpdate((e) => {
+			if (e.translationX < 0 && e.translationX > -110) {
+				translatedX.value = e.translationX;
+			}
+		})
+		.onEnd(() => {
+			if (translatedX.value < -90) {
+				translatedX.value = withTiming(-1000, { duration: 1500 });
+				runOnJS(props.handleDelete)();
+			}
+			translatedX.value = withTiming(0, { duration: 700 });
+		});
 
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ translateX: translatedX.value }],
-  }));
+	const animatedStyle = useAnimatedStyle(() => ({
+		transform: [{ translateX: translatedX.value }],
+	}));
 
-  return (
-    <View style={{ marginHorizontal: 20 }}>
-      {props.role === "teacher" && (
-        <View style={styles.deleteBackground}>
-          <MaterialIcons name="delete" size={28} color="white" />
-        </View>
-      )}
-      <GestureDetector gesture={panGesture}>
-        <Animated.View style={[animatedStyle]}>
-          <TouchableOpacity
-            onPress={handleNavigate}
-            style={styles.touchableOpacity}
-            activeOpacity={100}
-          >
-            <View style={styles.row}>
-              <View style={styles.yellowBulletin}></View>
-              <View style={styles.textContainer}>
-                <Text style={styles.title} numberOfLines={3}>
-                  {props.title}
-                </Text>
-              </View>
-              <View style={styles.rightSection}>
-                <Text style={styles.date}>
-                  {newDate} {props.time}
-                </Text>
-                <Entypo name="chevron-small-right" size={30} color="#aaa" />
-              </View>
-            </View>
-          </TouchableOpacity>
-        </Animated.View>
-      </GestureDetector>
-    </View>
-  );
+	return (
+		<View style={{ marginHorizontal: 20 }}>
+			{props.role === "teacher" && (
+				<View style={styles.deleteBackground}>
+					<MaterialIcons name="delete" size={28} color="white" />
+				</View>
+			)}
+			<GestureDetector gesture={panGesture}>
+				<Animated.View style={[animatedStyle]}>
+					<TouchableOpacity
+						onPress={handleNavigate}
+						style={styles.touchableOpacity}
+						activeOpacity={100}
+					>
+						<View style={styles.row}>
+							<View style={styles.yellowBulletin}></View>
+							<View style={styles.textContainer}>
+								<Text style={styles.title} numberOfLines={3}>
+									{props.title}
+								</Text>
+							</View>
+							<View style={styles.rightSection}>
+								<Text style={styles.date}>
+									{newDate} {props.time}
+								</Text>
+								<FontAwesome6 name="arrow-right-long" size={15} color="#000" />
+							</View>
+						</View>
+					</TouchableOpacity>
+				</Animated.View>
+			</GestureDetector>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  touchableOpacity: {
-    backgroundColor: "#fff",
-    padding: 15,
-    borderRadius: 10,
-    elevation: 5,
-  },
-  deleteBackground: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: "#FF5646",
-    paddingHorizontal: 30,
-    justifyContent: "center",
-    alignItems: "flex-end",
-    borderRadius: 10,
-    zIndex: 0,
-  },
-  row: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: "500",
-  },
-  date: {
-    fontSize: 12,
-    color: "#888",
-  },
-  rightSection: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-end",
-  },
-  yellowBulletin: {
-    backgroundColor: "#FFBF18",
-    height: 45,
-    width: "1.5%",
-    borderRadius: 100,
-    marginRight: 20,
-  },
-  textContainer: {
-    flex: 1,
-  },
-  icons: {
-    flexDirection: "row",
-  },
+	touchableOpacity: {
+		backgroundColor: "#fff",
+		padding: 15,
+		borderRadius: 20,
+		borderColor: "#ddd",
+		borderWidth: 1,
+		// elevation: 5,
+	},
+	deleteBackground: {
+		...StyleSheet.absoluteFillObject,
+		backgroundColor: "#FF5646",
+		paddingHorizontal: 30,
+		justifyContent: "center",
+		alignItems: "flex-end",
+		borderRadius: 10,
+		zIndex: 0,
+	},
+	row: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	title: {
+		fontSize: 14,
+		fontWeight: 500,
+	},
+	date: {
+		fontSize: 12,
+		color: "#888",
+		marginHorizontal: 10,
+	},
+	rightSection: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "flex-end",
+		marginHorizontal: 5,
+	},
+	yellowBulletin: {
+		backgroundColor: "#FFBF18",
+		height: 25,
+		width: "1.5%",
+		borderRadius: 100,
+		marginRight: 20,
+	},
+	textContainer: {
+		flex: 1,
+	},
+	icons: {
+		flexDirection: "row",
+	},
 });
 
 export default memo(announceCard);
