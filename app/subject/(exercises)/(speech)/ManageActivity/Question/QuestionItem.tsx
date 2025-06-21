032002b1e13159cc23e588877aae76382a65aa27
shironzi <aaronbaon1@gmail.com>
2025-06-21 23:21:04 +0800

    fix: flashcard update error

--- app/subject/(exercises)/(speech)/ManageActivity/Question/QuestionItem.tsx
+++ app/subject/(exercises)/(speech)/ManageActivity/Question/QuestionItem.tsx
@@ -61,100 +61,114 @@ const QuestionItem = ({
   const textLimit = hasError.some((item) => item.error === "text length");
 
   return (
-    <SafeAreaView
-      style={{ paddingBottom: 50, flex: 1,}}>
-          <View
-      style={[
-        {
-          marginHorizontal: 20,
-          padding: 20,
-          backgroundColor: "#fff",
-          rowGap: 10,
-          borderColor: "#ddd",
-          borderWidth: 1,
-          borderRadius: 20,
-          marginTop: -5,
-          marginVertical:-80,
-          flex:1,
-          // marginBottom:150
-        },
-        item.id === firstIndex && styles.itemTopRounded,
-        item.id === lastIndex && styles.itemBottomRounded,
-      ]}
-    >
-      {item.id === firstIndex && (
-        <View>
-          <Text style={[globalStyles.text1, {marginTop:-5}]}>Word Flashcards</Text>
-          <View style={[globalStyles.divider, {marginVertical:10, width:350, left:-10}]} />
-        </View>
-      )}
+    <SafeAreaView style={{ paddingBottom: 50, flex: 1 }}>
       <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
+        style={[
+          {
+            marginHorizontal: 20,
+            padding: 20,
+            backgroundColor: "#fff",
+            rowGap: 10,
+            borderColor: "#ddd",
+            borderWidth: 1,
+            borderRadius: 20,
+            marginTop: -5,
+            marginVertical: -80,
+            flex: 1,
+            // marginBottom:150
+          },
+          item.id === firstIndex && styles.itemTopRounded,
+          item.id === lastIndex && styles.itemBottomRounded,
+        ]}
       >
-        <Text style={globalStyles.text1}>Number {index + 1}</Text>
-        <TouchableOpacity onPress={() => handleRemove(item.id)}>
-          <AntDesign name="close" size={24} color="#aaa" />
-        </TouchableOpacity>
-      </View>
-      <View style={{ marginBottom: 10 }}>
-        <Text style={globalStyles.text1}>Word</Text>
-        {wordError ? (
-          <Text style={globalStyles.errorText}>This field is required</Text>
-        ) : (
-          textLimit && (
-            <Text style={globalStyles.errorText}>
-              This should not exceed 30 characters
+        {item.id === firstIndex && (
+          <View>
+            <Text style={[globalStyles.text1, { marginTop: -5 }]}>
+              Word Flashcards
             </Text>
-          )
-        )}
-        <TextInput
-          style={[
-            globalStyles.textInputContainer,
-            wordError && styles.errorBorder,
-            {marginVertical:5, marginBottom:-5}
-          ]}
-          placeholder={"E.g., 'bare' for 'bear'"}
-          value={item.text}
-          onChangeText={(value) => handleTextInput(item.id, value)}
-        />
-      </View>
-      {item.id === lastIndex && (
-        <View style={[globalStyles.divider, { marginVertical: 10, width: 350, left: -10 }]} />
-      )}
-      {item.id === lastIndex && (
-        <View style={styles.footerContainer}>
-          <TouchableOpacity style={styles.addItemRow} onPress={handleAdd}>
-            <MaterialIcons name="add" size={24} color="#FFBF18" />
-            <Text style={styles.addFileText}>Add Item</Text>
-          </TouchableOpacity>
-
-          <View style={{flexDirection:"row", justifyContent:"center", columnGap:10}}>
-          <TouchableOpacity
-              style={[globalStyles.inactivityButton, { width: "48%" }]}
-              onPress={() => router.back()}
-          >
-            <Text style={globalStyles.inactivityButtonText}> Cancel
-            </Text>
-          </TouchableOpacity>
-            <TouchableOpacity
-            style={[globalStyles.submitButton, {width:"48%"}]}
-            onPress={handlePreview}
-          >
-            <Text style={[globalStyles.submitButtonText, {top:3}]}>
-              {activityId ? "Update" : "Create"}
-            </Text>
-            </TouchableOpacity>
+            <View
+              style={[
+                globalStyles.divider,
+                { marginVertical: 10, width: 350, left: -10 },
+              ]}
+            />
           </View>
+        )}
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={globalStyles.text1}>Number {index + 1}</Text>
+          <TouchableOpacity onPress={() => handleRemove(item.id)}>
+            <AntDesign name="close" size={24} color="#aaa" />
+          </TouchableOpacity>
         </View>
-      )}
-    </View>
+        <View style={{ marginBottom: 10 }}>
+          <Text style={globalStyles.text1}>Word</Text>
+          {wordError ? (
+            <Text style={globalStyles.errorText}>This field is required</Text>
+          ) : (
+            textLimit && (
+              <Text style={globalStyles.errorText}>
+                This should not exceed 30 characters
+              </Text>
+            )
+          )}
+          <TextInput
+            style={[
+              globalStyles.textInputContainer,
+              wordError && styles.errorBorder,
+              { marginVertical: 5, marginBottom: -5 },
+            ]}
+            placeholder={"E.g., 'bare' for 'bear'"}
+            value={item.text}
+            onChangeText={(value) => handleTextInput(item.id, value)}
+          />
+        </View>
+        {item.id === lastIndex && (
+          <View
+            style={[
+              globalStyles.divider,
+              { marginVertical: 10, width: 350, left: -10 },
+            ]}
+          />
+        )}
+        {item.id === lastIndex && (
+          <View style={styles.footerContainer}>
+            <TouchableOpacity style={styles.addItemRow} onPress={handleAdd}>
+              <MaterialIcons name="add" size={24} color="#FFBF18" />
+              <Text style={styles.addFileText}>Add Item</Text>
+            </TouchableOpacity>
 
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                columnGap: 10,
+              }}
+            >
+              <TouchableOpacity
+                style={[globalStyles.inactivityButton, { width: "48%" }]}
+                onPress={() => router.back()}
+              >
+                <Text style={globalStyles.inactivityButtonText}> Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[globalStyles.submitButton, { width: "48%" }]}
+                onPress={handlePreview}
+              >
+                <Text style={[globalStyles.submitButtonText, { top: 3 }]}>
+                  {activityId ? "Update" : "Create"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      </View>
     </SafeAreaView>
-
   );
 };
 
